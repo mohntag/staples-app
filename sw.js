f23cabe09b1706910bc4dfe37101a81a4a2af4a0
mohntag <hello@georgoehler.co.uk>
2025-08-23 08:46:58 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'staples-v1';
+const CACHE = 'staples-v3';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./sw.js','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
